f964c62444de642ac0db61b3f24873549a081426
Spencer Carlson <spencerthecarlson@gmail.com>
2019-06-05 09:41:54 -0700

    set up dependecies

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars');
+var path = require('path');
+var twitData = require('./twitData')
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    
+});
