fdb2d277088be54cdeaa358771a2c29d44998948
Greg Oertli <oertli@10-248-148-236.wireless.oregonstate.edu>
2019-06-05 10:41:24 -0700

    basic page layout, css doesnt work

---
+++ server.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res, next) {
+    res.status(200).render('home');
+});
+
+app.get('*', function(req, res, next) {
+    res.status(404).render('404');
+});
+
+app.listen(port, function () {
+    console.log("== Server listening on port", port);
+});
