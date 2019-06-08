009542f3fad3b86570fda5e120bcc705b08704e0
Greg Oertli <oertli@Gregs-MacBook-Pro.local>
2019-06-08 15:10:34 -0700

    error with mongo

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ var exphbs = require('express-handlebars');
 
 // var twitData = require('./twitData') // no twit data??
 
+var MongoClient = require('mongodb').MongoClient;
+
 var app = express();
 var handlebars = require('express-handlebars');
 var path = require('path');
 var port = process.env.PORT || 3000;
+
+var MongoClient = require('mongodb').MongoClient;
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+var db = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -21,9 +33,19 @@ app.get('/todo', function(req, res, next) {
   res.status(200).render('ToDo');
 });
 
-app.listen(port, function () {
+MongoClient.connect(mongoUrl, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db(mongoDBName);
+  app.listen(port, function () {
     console.log("== Server listening on port", port);
+  });
 });
+
+// app.listen(port, function () {
+//     console.log("== Server listening on port", port);
+// });
 
 app.use(function(req,res){
   res.status(404);
