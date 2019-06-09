dbb4b39daeac26dfce0510bea1270771994ef56d
Greg Oertli <oertli@10-197-33-95.sds.oregonstate.edu>
2019-06-08 18:20:33 -0700

    Mongo is set up, make sure you are connected to OSU via VPN and have the enviromental variables set up

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ var port = process.env.PORT || 3000;
 
 var todoData = require('./todoData');
 
-// var MongoClient = require('mongodb').MongoClient;
-// var mongoHost = process.env.MONGO_HOST;
-// var mongoPort = process.env.MONGO_PORT || 27017;
-// var mongoUser = process.env.MONGO_USER;
-// var mongoPassword = process.env.MONGO_PASSWORD;
-// var mongoDBName = process.env.MONGO_DB_NAME;
+var MongoClient = require('mongodb').MongoClient;
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
 
-// var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
-// var db = null;
+var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+var db = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -38,19 +38,20 @@ app.get('/todo', function(req, res, next) {
   });
 });
 
-// MongoClient.connect(mongoUrl, function (err, client) {
-//   if (err) {
-//     throw err;
-//   }
-//   db = client.db(mongoDBName);
-//   app.listen(port, function () {
-//     console.log("== Server listening on port", port);
-//   });
-// });
-
-app.listen(port, function () {
+MongoClient.connect(mongoUrl, function (err, client) {
+  if (err) {
+    console.log(mongoUrl);
+    throw err;
+  }
+  db = client.db(mongoDBName);
+  app.listen(port, function () {
     console.log("== Server listening on port", port);
+  });
 });
+
+// app.listen(port, function () {
+//     console.log("== Server listening on port", port);
+// });
 
 app.use(function(req,res){
   res.status(404);
