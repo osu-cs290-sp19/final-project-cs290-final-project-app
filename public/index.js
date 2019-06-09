b217d59c888eccd04e44b97b59ac05749dc1d5c4
Greg Oertli <oertli@10-197-34-97.sds.oregonstate.edu>
2019-06-09 15:00:43 -0700

    Create Todo button

---
+++ public/index.js
@@ -0,0 +1,6 @@
+function showCreateTodo() {
+    console.log("show the create todo");
+}
+
+var createTwitButton = document.getElementById('create-todo-button');
+createTwitButton.addEventListener('click', showCreateTodo);
