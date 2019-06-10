65eb40aa050c5428ebc55d9da82c82c187c40023
Greg Oertli <oertli@10-197-34-97.sds.oregonstate.edu>
2019-06-09 17:57:06 -0700

    Create Todo displays modal and hides modal

--- public/index.js
+++ public/index.js
@@ -1,6 +1,27 @@
+
+var createTodoButton = document.getElementById('create-todo-button');
+createTodoButton.addEventListener('click', showCreateTodo);
+
 function showCreateTodo() {
-    console.log("show the create todo");
+    console.log("showCreateTodo");
+    var modal = document.querySelector('#modal-backdrop');
+    var createTodo = document.querySelector('#create-todo-modal');
+
+    modal.classList.remove('hidden');
+    createTodo.classList.remove('hidden');
 }
 
-var createTwitButton = document.getElementById('create-todo-button');
-createTwitButton.addEventListener('click', showCreateTodo);
+var closeTodoButton = document.querySelector('.modal-close-button');
+closeTodoButton.addEventListener('click', hideCreateTodo);
+
+var cancelTodoButton = document.querySelector('.modal-cancel-button');
+cancelTodoButton.addEventListener('click', hideCreateTodo);
+
+function hideCreateTodo() {
+    console.log('hideCreateTodo');
+    var modal = document.querySelector('#modal-backdrop');
+    var createTodo = document.querySelector('#create-todo-modal');
+
+    modal.classList.add('hidden');
+    createTodo.classList.add('hidden');
+}
